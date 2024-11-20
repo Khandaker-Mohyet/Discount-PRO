426a2d9715c85e7fdd333a3f8bae085ea51e34ea
Khandaker-Mohyet <mohyet008@gmail.com>
2024-11-20 23:00:36 +0600

    details section doen

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,15 +1,19 @@
 
 import { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const Details = () => {
   const data = useLoaderData()
   const { id } = useParams()
-  const [product, setProduct] = useState({})
-  const {brand_name, brand_logo, rating, coupons} = product || {}
+  const [product, setProduct] = useState(null)
+  const [isCopied, setIsCopied] = useState(false);
 
-  console.log(coupons)
-
+  const handleCopy = () => {
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
+  };
+  
   useEffect(() => {
 
     const singleData = data.find(product => product.id == id)
@@ -17,17 +21,51 @@ const Details = () => {
     
   }, [data, id])
 
+  const { brand_name, brand_logo, rating, coupons } = product || {}
+  
+  console.log(product, coupons)
+
   return (
     <div>
-      <h1 className='text-2xl font-bold'>{brand_name}</h1>
-      <img src={brand_logo} alt="" />
+      <div className='flex flex-col justify-center items-center'>
+        <img className='max-w-[50%]' src={brand_logo} alt="" />
+        <h1 className='text-2xl font-bold text-blue-600'>{brand_name}</h1>
+      <p>Rating: <div className="rating rating-sm">
+            <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+            <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+            <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+  
+            <input
+            type="radio"
+            name="rating-6"
+            className="mask mask-star-2 bg-orange-400"
+              defaultChecked />
+            <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-400" />
+  
+         </div> {rating}</p>
+      </div>
         
-      <div>
-      {/* {
+      <div className="md: flex my-14 w-8/12 mx-auto">
+      { coupons?.length>0 &&
        coupons.map((coupon, idx) => (
-       <div key={idx}>{coupon.coupon_code}</div>
+         <div className="card bg-base-200 shadow-md w-96 p-5 mx-auto space-y-3" key={idx}>
+           
+           <h1 className='font-bold'>Coupon: {coupon.coupon_code}</h1>
+           <p>Description: {coupon.description}</p>
+           <p>Expiry date: {coupon.expiry_date}</p>
+           <p>Condition: {coupon.condition}</p>
+           <p>Coupon type: {coupon.coupon_type}</p>
+           <CopyToClipboard text={coupon.coupon_code} onCopy={handleCopy}>
+        <button className="btn btn-primary btn-outline my-2">Copy Code</button>
+      </CopyToClipboard>
+      {isCopied && (
+        <p className="text-green-500 mt-2">Coupon code copied successfully!</p>
+      )}
+           
+           
+         </div>
       ))
-      } */}
+      }
       </div>
     </div>
   );
